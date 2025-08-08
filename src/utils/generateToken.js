27b376db21c77b1be9baa9d39c7db5181dd5b9f6
Kent Adriane Goc-ong <kentadriane2@gmail.com>
2025-08-08 23:33:15 +0800

    fix: update secure cookie setting to true for production environment

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -7,7 +7,7 @@ export const generateToken = (user, res) => {
 
   res.cookie("chat_token", token, {
     httpOnly: true,
-    secure: false, // true if using HTTPS
+    secure: true, // true if using HTTPS
     sameSite: "None", // "None" if not same host change to "lax" for developments
   });
   
