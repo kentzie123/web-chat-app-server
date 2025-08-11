b6c2d022cdb4208edc7d1a3f13d4faf3068839f8
Kent Adriane Goc-ong <kentadriane2@gmail.com>
2025-08-11 19:37:29 +0800

    added socket on incoming call

--- src/server.js
+++ src/server.js
@@ -1,51 +1,53 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express';
-import http from 'http';
-import { Server as SocketIO } from 'socket.io';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import http from "http";
+import { Server as SocketIO } from "socket.io";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 // Routes
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/messages.route.js';
-import userRoutes from './routes/user.route.js';
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/messages.route.js";
+import userRoutes from "./routes/user.route.js";
 
 // Supabase connection
 // import supabase from './config/db.js';
 
-const allowedOrigins = ['https://chattrixx.netlify.app', 'http://192.168.1.5:5174'];
+const allowedOrigins = [
+  "https://chattrixx.netlify.app",
+  "http://192.168.1.5:5174",
+];
 
 const app = express();
 const server = http.createServer(app);
 export const io = new SocketIO(server, {
   cors: {
     origin: allowedOrigins,
-    credentials: true
+    credentials: true,
   },
 });
 
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
-app.use(express.json({limit: '15mb'}));
-app.use(cookieParser()); 
-
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "15mb" }));
+app.use(cookieParser());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
-app.use('/api/users', userRoutes);
-
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // supabase
 // .channel('public:messages')
@@ -61,37 +63,25 @@ app.use('/api/users', userRoutes);
 //   }
 // ).subscribe();
 
-
 const PORT = process.env.PORT || 5000;
-
 
 export const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
-
-  socket.on("join-room", (roomId) => {
-    socket.join(roomId);
-  });
-
-  socket.on("offer", ({ offer, roomId }) => {
-    socket.to(roomId).emit("offer", offer);
-  });
-
-  socket.on("answer", ({ answer, roomId }) => {
-    socket.to(roomId).emit("answer", answer);
-  });
-
-  socket.on("ice-candidate", ({ candidate, roomId }) => {
-    socket.to(roomId).emit("ice-candidate", candidate);
-  });
-
   console.log("A user connected", socket.id);
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
   console.log(userSocketMap);
-  
+
   // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("call-user", ({ targetId }) => {
+    io.to(userSocketMap[targetId]).emit("incoming-call", {
+      fromSocketId: socket.id,
+      fromUserId: userId
+    })
+  });
 
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
@@ -100,6 +90,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
